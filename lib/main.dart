e33e216e4ca85a8b2868a5aec1e36c4c6bb655a4
Won Jae Kim <86081049+Daryu-Kim@users.noreply.github.com>
2023-04-18 14:28:38 +0900

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+    ).then((value) => print(value));
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
