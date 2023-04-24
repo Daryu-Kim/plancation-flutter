40b4afc798d371161c4c2b27c0ed3509a17243e5
Won Jae Kim <86081049+Daryu-Kim@users.noreply.github.com>
2023-04-24 18:57:58 +0900

    Update

--- lib/pages/home_calendar.dart
+++ lib/pages/home_calendar.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import '../components/calendar.dart';
 
 class HomeCalendarComponent extends StatelessWidget {
+  const HomeCalendarComponent({super.key});
+
+  void refresh() {
+    print("Refresh!");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
