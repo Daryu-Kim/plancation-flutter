12df883a849f7d5bbe155115b1457accd194b6cd
Won Jae Kim <86081049+Daryu-Kim@users.noreply.github.com>
2023-05-03 19:03:14 +0900

    Update

--- lib/pages/home_calendar/home_calendar.dart
+++ lib/pages/home_calendar/home_calendar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:logger/logger.dart';
 import 'package:plancation/pages/home_calendar/home_calendar.style.dart';
@@ -63,12 +64,45 @@ class HomeCalendarComponent extends StatelessWidget {
           vertical: 16
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () => Logger().d("Add!"),
-          child: Icon(Icons.add, color: Theme.of(context).colorScheme.inversePrimary),
-        shape: CircleBorder(side: BorderSide(color: Theme.of(context).colorScheme.primary)),
+      floatingActionButton: SpeedDial(
         backgroundColor: Theme.of(context).colorScheme.primary,
-      ),
+        child: Icon(Icons.add, color: Theme.of(context).colorScheme.surface,),
+        activeChild: Icon(Icons.close, color: Theme.of(context).colorScheme.surface,),
+        spacing: 20,
+        onOpen: () => Logger().d("열림"),
+        onClose: () => Logger().d("닫힘"),
+        children: [
+          SpeedDialChild(
+            shape: CircleBorder(),
+            label: "  할일 추가  ",
+            labelStyle: TextStyle(
+              color: Theme.of(context).colorScheme.surface,
+              fontSize: 16,
+              fontWeight: FontWeight.w500
+            ),
+            labelBackgroundColor: Theme.of(context).colorScheme.primary,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            foregroundColor: CupertinoColors.white,
+            child: SvgPicture.asset('assets/svgs/todo_add.svg', color: Theme.of(context).colorScheme.surface,),
+            onTap: () => Logger().d("할일 추가 클릭됨."),
+          ),
+          SpeedDialChild(
+            shape: CircleBorder(),
+            label: "  일정 추가  ",
+            labelStyle: TextStyle(
+                color: Theme.of(context).colorScheme.surface,
+                fontSize: 16,
+                fontWeight: FontWeight.w500
+            ),
+            labelBackgroundColor: Theme.of(context).colorScheme.primary,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            foregroundColor: CupertinoColors.white,
+            child: SvgPicture.asset('assets/svgs/calendar_add.svg', color: Theme.of(context).colorScheme.surface,),
+            onTap: () => Logger().d("일정 추가 클릭됨."),
+          ),
+        ],
+      )
+
     );
   }
 }
