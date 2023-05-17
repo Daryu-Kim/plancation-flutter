fdda3f01579088b631f46ec7c694c37c8cf3f482
Daryu <dev_daryu@kakao.com>
2023-05-17 13:59:12 +0900

    update

--- lib/pages/home_todo/home_todo.dart
+++ lib/pages/home_todo/home_todo.dart
@@ -1,6 +1,10 @@
+import 'dart:core';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:plancation/pages/home_todo/home_todo.style.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+DateTime now = DateTime.now();
 
 class HomeTodoPage extends StatefulWidget {
   const HomeTodoPage({super.key});
@@ -10,9 +14,14 @@ class HomeTodoPage extends StatefulWidget {
 }
 
 class _HomeTodoPageState extends State<HomeTodoPage> {
+  DateTime firstDay = DateTime(now.year, now.month - 3, now.day);
+  DateTime lastDay = DateTime(now.year, now.month + 3, now.day);
+
   @override
   void initState() {
     super.initState();
+    setState(() {
+    });
   }
 
   @override
@@ -41,7 +50,43 @@ class _HomeTodoPageState extends State<HomeTodoPage> {
         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
         child: SizedBox(
           height: MediaQuery.of(context).size.height - 172,
-          child: const Text("TODO")
+          child: Column(
+            children: [
+              TableCalendar(
+                focusedDay: now,
+                firstDay: firstDay,
+                lastDay: lastDay,
+                calendarFormat: CalendarFormat.week,
+                // 시작요일은 월요일
+                startingDayOfWeek: StartingDayOfWeek.monday,
+
+                headerStyle: const HeaderStyle(
+                  leftChevronIcon: Icon(
+                    Icons.chevron_left,
+                    size: 24,
+                  ),
+                  rightChevronIcon: Icon(
+                    Icons.chevron_right,
+                    size: 24,
+                  ),
+                  headerPadding: EdgeInsets.zero,
+                  formatButtonVisible: true,
+                ),
+                calendarStyle: CalendarStyle(
+                  selectedDecoration: const BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.rectangle,
+                  ),
+                  selectedTextStyle:
+                  const TextStyle(color: Color.fromRGBO(238, 230, 226, 1)),
+                  todayDecoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.tertiary,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+            ],
+          )
         ),
       ),
     );
